e2df2fc2bc984c5501b0cd8138755a9dabb6524b
KaiaWalters <kaia.cwalters@gmail.com>
2024-12-21 10:42:20 -0500

    Adds to more js wars

---
+++ Javascript/product_of_consecutive_fib_nums.js
@@ -0,0 +1,22 @@
+
+function productFib(prod) {
+   a = 0 
+   b = 1 
+
+   while((a * b) < prod){
+        let next = a + b 
+        a = b 
+        b = next; 
+   }
+   console.log([a, b, a * b === prod])
+   return [a, b, a * b === prod];
+}
+   
+productFib(714)
+
+
+//Take aways 
+
+// I need to get better at working with pointers 
+// verifying truthy and valsey statements in place instead of assigning variables 
+// other types of loops besides for loops :(, avoing superfluos loops 
